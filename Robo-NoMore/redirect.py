325765aa73e0c8234d61d2e61f8db7c89be68505
【★༒✞︎©༒SATYA-OP】MR-SATYA-OP <104726886+SATYA-OP@users.noreply.github.com>
2023-01-16 08:47:43 -0800

    Create redirect.py

---
+++ Robo-NoMore/redirect.py
@@ -0,0 +1,7 @@
+from redirect import nomore
+def program 
+   install()
+  run("git clone:KJHGT67657HGJJF9G99D9CC9CCC977GG7BDD8GFDSH8BS8SN8D7NV7NB77FB:\00900988787\HDSHGI\loding"),git
+      program("run")
+      legend(,mark.downloads:pro),
+      print(:"{run}":).os.import(/start)
